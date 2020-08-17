1f3311e6f9beaf91aa5ccc805676afb48234c04e
Matheus Teles <matheustdo1@gmail.com>
2020-08-17 14:38:41 -0300

    Add navbar background color change

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ $("a").on("click", function (event) {
     400
   );
 });
+
+$(".sectionWrapper").scroll(function () {
+  var navHeight = $(".navbar").outerHeight();
+
+  if (0 >= $("#sectionContact").offset().top - navHeight) {
+    $("#navbarBackground").css("background-color", "transparent");
+  } else if (0 >= $("#sectionTeam").offset().top - navHeight) {
+    $("#navbarBackground").css("background-color", "#1f1f1f");
+  } else if (0 >= $("#sectionProjects").offset().top - navHeight) {
+    $("#navbarBackground").css("background-color", "#2a2a2a");
+  } else if (0 >= $("#sectionAbout").offset().top - navHeight) {
+    $("#navbarBackground").css("background-color", "#1f1f1f");
+  } else {
+    $("#navbarBackground").css("background-color", "transparent");
+  }
+});
